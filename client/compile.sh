07087b64e573789c4396b2bd6e652e95d9837cf4
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-07-13 21:05:24 -0700

    Add blank client

---
+++ client/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+javac -cp bin/minecraft.jar:minecraftforge-client-3.3.8.152.zip mod_Squirt.java
---
+++ client/mod_Squirt.java
@@ -0,0 +1,32 @@
+
+import forge.*;
+
+class BlockExample extends pb/*Block*/ {
+    protected BlockExample(int i, int j, acn/*Material*/ material) {
+        super(i, j, material);
+    }
+}
+
+public class mod_Squirt extends NetworkMod {
+    public mod_Squirt() {
+        pb/*Block*/ block = new BlockExample(255, 0, acn.e/*Material.something*/).c(3F).b(10F).a("Example");
+        ModLoader.registerBlock(block);
+    }
+
+    public void load() {
+        System.out.println("loading mod_Squirt!");
+    }
+
+    public String getVersion() {
+        return "0";
+    }
+
+    public boolean clientSideRequired() {
+        return false;
+    }
+
+    public boolean serverSideRequired() {
+        return false;
+    }
+}
+
